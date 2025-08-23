459fcfdb9bf535914cf287b0833637c09540911d
fahadsalie <41153085+fahadsalie@users.noreply.github.com>
2025-08-23 22:40:07 +1000

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
-export const dynamic = 'force-dynamic'; // add this at top
+'use client';                         // must be first line
+export const dynamic = 'force-dynamic';
 
-'use client';
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
 import { useAuth } from '@/components/AuthProvider';
